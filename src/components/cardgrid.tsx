9df3760d8f2160c9b1f4df87fce72a16dd8d066e
Fooxless <n10776800@qut.edu.au>
2022-12-23 15:34:17 +1000

    Footer changes and page movement

--- src/components/cardgrid.tsx
+++ src/components/cardgrid.tsx
@@ -5,35 +5,68 @@ import { useState, useEffect } from 'react'
 export default function CardGrid() {
     const [monsters, setData] = useState<any>(null)
     const [isLoading, setLoading] = useState(false)
+    const [page, setPage] = useState(1)
 
     useEffect(() => {
         setLoading(true)
-        fetch('/api/monsters')
+        fetch('/api/monsters', { method: 'POST', body: JSON.stringify({ page: page }) })
             .then((res) => res.json())
             .then((data) => {
                 setData(data)
                 setLoading(false)
             })
-    }, [])
+    }, [page])
 
-    console.log("data", monsters)
+    console.log("monsterdata", monsters)
 
+    function clickForward() {
+        setPage(page + 1);
+        console.log('You clicked forward.');
+    }
+
+    function clickBack() {
+        if (page !== 1) {
+            setPage(page - 1);
+        }
+        console.log('You clicked back.');
+    }
 
 
     return (
         <div>
             {isLoading ? (
-                <svg aria-hidden="true" className="mr-2 w-24 h-24 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
-                </svg>)
-                : (<div className='grid items-grid gap-8 px-6 place-items-center pt-10'>
+                <div className="flex justify-center pt-80">
+                    <svg aria-hidden="true" className="w-24 h-24 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                    </svg>
+                </div>)
+                : (<div>
+                    <div className='grid items-grid gap-8 px-6 place-items-center pt-10'>
 
-                    {monsters?.results.map((monster: any) => (
-                        <Card key={monster.index} monster={monster} />
-                    ))}
+                        {monsters?.results.map((monster: any) => (
+                            <Card key={monster.index} monster={monster} />
+                        ))}
 
+                    </div>
+                    <div className='flex justify-center dark:text-gray-50 mt-8'>
+                        {page === 1 ? (<svg className="mx-10 dark:text-gray-500" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd"
+                            clip-rule="evenodd" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"
+                            onClick={clickBack}>
+                            <path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm3 5.753l-6.44 5.247 6.44 5.263-.678.737-7.322-6 7.335-6 .665.753z" /></svg>)
+                            : (<svg className="mx-10 cursor-pointer" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd"
+                                clip-rule="evenodd" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"
+                                onClick={clickBack}>
+                                <path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm3 5.753l-6.44 5.247 6.44 5.263-.678.737-7.322-6 7.335-6 .665.753z" /></svg>)}
+
+                        Page {page}
+                        <svg className="mx-10 cursor-pointer" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd"
+                            clip-rule="evenodd" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"
+                            onClick={clickForward}>
+                            <path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm-3 5.753l6.44 5.247-6.44 5.263.678.737 7.322-6-7.335-6-.665.753z" /></svg>
+                    </div>
                 </div>)}
+
 
         </div>
     )
--- src/pages/api/monsters.ts
+++ src/pages/api/monsters.ts
@@ -1,13 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 const axios = require('axios');
 
-const url = "https://api.open5e.com/monsters/?ordering=challenge_rating";
+
 
 export default async function monsters(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== "GET") {
+  if (req.method !== "POST") {
     return res.status(405).json({ message: "Method not allowed" });
   }
   else {
+    const data = JSON.parse(req.body);
+    const url = `https://api.open5e.com/monsters/?ordering=challenge_rating&page=${data.page}`;
+    console.log("data", data);
     await axios.get(url, { headers: { "Accept-Encoding": "gzip,deflate,compress" } })
       .then(function (response: any) {
         // handle success
